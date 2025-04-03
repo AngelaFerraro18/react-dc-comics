d2cd694f380d9545b8d667af2b1321afa5d3f6c3
AngelaFerraro18 <angela.ferraro1802@gmail.com>
2025-04-03 14:39:30 +0200

    feat: create Menu component

---
+++ react-dc-comics/src/components/Menu.jsx
@@ -0,0 +1,16 @@
+function Menu() {
+    return (<ul>
+        <li>Characters</li>
+        <li>Comics</li>
+        <li>Movies</li>
+        <li>TV</li>
+        <li>Games</li>
+        <li>Collectibles</li>
+        <li>Videos</li>
+        <li>Fans</li>
+        <li>News</li>
+        <li>Shop</li>
+    </ul>)
+}
+
+export default Menu;
