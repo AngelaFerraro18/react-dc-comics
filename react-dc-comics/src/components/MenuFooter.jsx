94ea52f93d85b1ef50795752fbbfd06e1d31ce7e
AngelaFerraro18 <angela.ferraro1802@gmail.com>
2025-04-03 15:15:23 +0200

    feat: create MenuFooter component

---
+++ react-dc-comics/src/components/MenuFooter.jsx
@@ -0,0 +1,40 @@
+//creo la funzione per il menu del footer
+function MenuFooter() {
+    return (<ul>
+        <h2>DC Comics</h2>
+        <li>Characters</li>
+        <li>Comics</li>
+        <li>Movies</li>
+        <li>TV</li>
+        <li>Games</li>
+        <li>Videos</li>
+        <li>News</li>
+
+        <h2>Shop</h2>
+        <li>Shop DC</li>
+        <li>Shop DC Collectibles</li>
+
+        <h2>DC</h2>
+        <li>Terms Of Use</li>
+        <li>Privacy Policy(New)</li>
+        <li>Ad Choices</li>
+        <li>Advertising</li>
+        <li>Jobs</li>
+        <li>Subscriptions</li>
+        <li>Talent Workshops</li>
+        <li>CPSC Certificates</li>
+        <li>Ratings</li>
+        <li>Shop Help</li>
+        <li>Contact Us</li>
+
+        <h2>Sites</h2>
+        <li>DC</li>
+        <li>MAD Magazine</li>
+        <li>DC Kids</li>
+        <li>DC Universe</li>
+        <li>DC Power Visa</li>
+    </ul>)
+}
+
+//esporto
+export default MenuFooter;
