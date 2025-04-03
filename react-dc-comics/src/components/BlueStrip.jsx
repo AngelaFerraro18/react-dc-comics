f90817714071b56b94059ebb24fc7623e9b09ffe
AngelaFerraro18 <angela.ferraro1802@gmail.com>
2025-04-03 16:03:38 +0200

    feat: correction icon position

--- react-dc-comics/src/components/BlueStrip.jsx
+++ react-dc-comics/src/components/BlueStrip.jsx
@@ -6,11 +6,11 @@ import digitalPowerVisa from "../assets/img/buy-dc-power-visa.svg";
 
 //creo la funzione che raggruppa la lista di icone
 function BlueStrip() {
-    return (<ul className="container">
+    return (<ul id="blue-strip" className="container">
         <li><img src={digitalComics} alt="digital-comics" />Digital Comics</li>
         <li><img src={digitalMerchandise} alt="merchandise" />DC Merchandise</li>
-        <li><img src={digitalShop} alt="shop" />Subscriptions</li>
-        <li><img src={digitalSubscriptions} alt="subscriptions" />Comic Shop Locator</li>
+        <li><img src={digitalSubscriptions} alt="subscriptions" />Subscriptions</li>
+        <li><img src={digitalShop} alt="shop" />Comic Shop Locator</li>
         <li><img src={digitalPowerVisa} alt="power-visa" />DC Power Visa</li>
     </ul>)
 }
