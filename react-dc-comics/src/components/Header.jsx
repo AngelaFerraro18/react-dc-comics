8ed5a1672de55a57047c51512ba268b7d25f81c6
AngelaFerraro18 <angela.ferraro1802@gmail.com>
2025-04-03 14:39:46 +0200

    feat: import Logo and Menu in Header

--- react-dc-comics/src/components/Header.jsx
+++ react-dc-comics/src/components/Header.jsx
@@ -1,6 +1,12 @@
+import Logo from "./Logo";
+import Menu from "./Menu";
+
 //creo la funzione che mi identificherà l'header del documento
 function Header() {
-    return (<header>Sono un header</header>);
+    return (<header>
+        <Logo />
+        <Menu />
+    </header>);
 };
 
 //esporto la funzione header
