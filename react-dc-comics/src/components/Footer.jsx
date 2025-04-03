75b3b8b8a928bb4043d0b53432fca54fca8e2c26
AngelaFerraro18 <angela.ferraro1802@gmail.com>
2025-04-03 15:17:43 +0200

    feat: import LogoBG and MenuFooter, then added btn and social-icons list

--- react-dc-comics/src/components/Footer.jsx
+++ react-dc-comics/src/components/Footer.jsx
@@ -1,6 +1,26 @@
+import LogoBG from "./LogoBG";
+import MenuFooter from "./MenuFooter";
+import facebookIcon from "../assets/img/footer-facebook.png";
+import twitterIcon from "../assets/img/footer-facebook.png";
+import youtubeIcon from "../assets/img/footer-youtube.png";
+import pinterestIcon from "../assets/img/footer-pinterest.png";
+import periscopeIcon from "../assets/img/footer-periscope.png";
+
 //creo la funzione che mi identificherà il footer del documento
 function Footer() {
-    return (<footer>Sono un footer</footer>);
+    return (<footer>
+        <MenuFooter />
+        <LogoBG />
+        <button>Sign-up Now</button>
+        <h3>Follow Us</h3>
+        <ul>
+            <li><img src={facebookIcon} alt="facebook-icon" /></li>
+            <li><img src={twitterIcon} alt="twitter-icon" /></li>
+            <li><img src={youtubeIcon} alt="youtube-icon" /></li>
+            <li><img src={pinterestIcon} alt="pinterest-icon" /></li>
+            <li><img src={periscopeIcon} alt="periscope-icon" /></li>
+        </ul>
+    </footer>);
 };
 
 //esporto la funzione Footer
