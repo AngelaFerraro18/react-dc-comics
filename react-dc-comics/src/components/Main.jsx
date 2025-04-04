9da8f095b1326f7058bc4f5deb9b12f04215824f
AngelaFerraro18 <angela.ferraro1802@gmail.com>
2025-04-04 17:19:00 +0200

    feat: add placeholder to empty main black strip

--- react-dc-comics/src/components/Main.jsx
+++ react-dc-comics/src/components/Main.jsx
@@ -3,7 +3,9 @@ import BlueStrip from "./BlueStrip";
 //creo la funzione che mi identificherà il main del documento
 function Main() {
     return (<main >
-        <div className="black-div"></div>
+        <div className="black-div">
+            <p className="container">{'---> Content Goes Here! <---'}</p>
+        </div>
         <BlueStrip />
     </main>);
 };
