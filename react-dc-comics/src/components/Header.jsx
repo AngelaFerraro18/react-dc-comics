6d5bdba15f29f32521a562d48f4cf3980dae2a06
AngelaFerraro18 <angela.ferraro1802@gmail.com>
2025-04-04 15:01:36 +0200

    feat: create an array with obj to describe all the links required and then apply props to Menu component

--- react-dc-comics/src/components/Header.jsx
+++ react-dc-comics/src/components/Header.jsx
@@ -3,9 +3,57 @@ import Menu from "./Menu";
 
 //creo la funzione che mi identificherà l'header del documento
 function Header() {
+
+    //creo l'array di oggetti dove ho impostato due proprietà: link, url
+    const linkList = [
+        {
+            link: 'Characters',
+            url: '/'
+        },
+        {
+            link: 'Comics',
+            url: '/'
+        },
+        {
+            link: 'Movies',
+            url: '/'
+        },
+        {
+            link: 'Tv',
+            url: '/'
+        },
+        {
+            link: 'Games',
+            url: '/'
+        },
+        {
+            link: 'Collectibles',
+            url: '/'
+        },
+        {
+            link: 'Videos',
+            url: '/'
+        },
+        {
+            link: 'Fans',
+            url: '/'
+        },
+        {
+            link: 'News',
+            url: '/'
+        },
+        {
+            link: 'Shop',
+            url: '/'
+        }
+    ]
+
+
     return (<header className="container">
         <Logo />
-        <Menu />
+
+        {/* ho impostato all'interno del mio componente l'attributo linkList e vi ho assegnato l'array che ho creato precedentemente */}
+        <Menu linkList={linkList} />
     </header>);
 };
 
