4e8907fecf5131c36e0249970595a52783a03c9b
AngelaFerraro18 <angela.ferraro1802@gmail.com>
2025-04-04 16:09:12 +0200

    feat: create arrays for every list and put attr to footer menu components

--- react-dc-comics/src/components/MenuFooter.jsx
+++ react-dc-comics/src/components/MenuFooter.jsx
@@ -6,13 +6,126 @@ import List4 from "./MenuFooter4";
 
 //creo la funzione per il menu del footer
 function MenuFooter() {
+
+    const list = [
+        {
+            link: 'Characters',
+            url: '/'
+        },
+        {
+            link: 'Comics',
+            url: '/'
+        },
+        {
+            link: 'Movies',
+            url: '/'
+        },
+        {
+            link: 'TV',
+            url: '/'
+        },
+        {
+            link: 'Games',
+            url: '/'
+        },
+        {
+            link: 'Videos',
+            url: '/'
+        },
+        {
+            link: 'News',
+            url: '/'
+        }
+
+    ];
+
+    const list2 = [{
+
+        link: 'Shop DC',
+        url: '/'
+    },
+    {
+        link: 'Shop DC Collectibles',
+        url: '/'
+
+    }]
+
+    const list3 = [{
+        link: 'Term Of Use',
+        url: '/'
+    },
+    {
+        link: 'Privacy Policy(New)',
+        url: '/'
+    },
+    {
+        link: 'AD Choices',
+        url: '/'
+    },
+    {
+        link: 'Advertising',
+        url: '/'
+    },
+    {
+        link: 'Jobs',
+        url: '/'
+    },
+    {
+        link: 'Subscriptions',
+        url: '/'
+    },
+    {
+        link: 'Talent Workshops',
+        url: '/'
+    },
+    {
+        link: 'CPSC Certificates',
+        url: '/'
+    },
+    {
+        link: 'Rating',
+        url: '/'
+    },
+    {
+        link: 'Shop Help',
+        url: '/'
+    },
+    {
+        link: 'Contact Us',
+        url: '/'
+    }
+    ]
+
+    const list4 = [
+        {
+            link: 'DC',
+            url: '/'
+        },
+        {
+            link: 'MAD Magazine',
+            url: '/'
+        },
+        {
+            link: 'DC Kids',
+            url: '/'
+        },
+        {
+            link: 'DC Universe',
+            url: '/'
+        },
+        {
+            link: 'DC Power Visa',
+            url: '/'
+        }
+    ]
+
     return (<div className="container-box-list">
         <div className="menu-footer container">
             <div className="d-flex">
-                <List1 />
-                <List2 />
-                <List3 />
-                <List4 />
+                <List1 list={list} />
+                <List2 list={list2} />
+                <List3 list={list3} />
+                <List4 list={list4} />
             </div>
             <LogoBG />
 
