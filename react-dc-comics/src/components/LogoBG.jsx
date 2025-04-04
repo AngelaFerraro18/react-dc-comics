fcf4608b9a2729c1d17c66ec0b04024c732081fc
AngelaFerraro18 <angela.ferraro1802@gmail.com>
2025-04-04 17:18:15 +0200

    feat: add class to work on image

--- react-dc-comics/src/components/LogoBG.jsx
+++ react-dc-comics/src/components/LogoBG.jsx
@@ -1,6 +1,6 @@
 //creo la funzione per il logo grande
 function LogoBG() {
-    return <img src="/dc-logo-bg.png" alt="logo-dc-comics-grande" />
+    return <img src="/dc-logo-bg.png" alt="logo-dc-comics-grande" className="image-logo-bg" />
 }
 //esporto
 export default LogoBG;
