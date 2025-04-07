29bdff25053d9d199f34ea38176ffe20e7eac7c7
AngelaFerraro18 <angela.ferraro1802@gmail.com>
2025-04-07 19:02:18 +0200

    feat: modify header menu applying map to recreate the same menu

--- react-dc-comics/src/components/Header.jsx
+++ react-dc-comics/src/components/Header.jsx
@@ -1,5 +1,6 @@
 import Logo from "./Logo";
-import Menu from "./Menu";
+import MenuHeader from "./Menu";
+// import Menu from "./Menu";
 
 //creo la funzione che mi identificherà l'header del documento
 function Header() {
@@ -7,42 +8,52 @@ function Header() {
     //creo l'array di oggetti dove ho impostato due proprietà: link, url
     const linkList = [
         {
+            id: '1',
             link: 'Characters',
             url: '/'
         },
         {
+            id: '2',
             link: 'Comics',
             url: '/'
         },
         {
+            id: '3',
             link: 'Movies',
             url: '/'
         },
         {
+            id: '4',
             link: 'Tv',
             url: '/'
         },
         {
+            id: '5',
             link: 'Games',
             url: '/'
         },
         {
+            id: '6',
             link: 'Collectibles',
             url: '/'
         },
         {
+            id: '7',
             link: 'Videos',
             url: '/'
         },
         {
+            id: '8',
             link: 'Fans',
             url: '/'
         },
         {
+            id: '9',
             link: 'News',
             url: '/'
         },
         {
+            id: '10',
             link: 'Shop',
             url: '/'
         }
@@ -54,7 +65,7 @@ function Header() {
             <Logo />
 
             {/* ho impostato all'interno del mio componente l'attributo linkList e vi ho assegnato l'array che ho creato precedentemente */}
-            <Menu linkList={linkList} />
+            <MenuHeader linkList={linkList} />
         </div>
     </header>);
 };
--- react-dc-comics/src/components/Menu.jsx
+++ react-dc-comics/src/components/Menu.jsx
@@ -1,34 +1,9 @@
-//assegno il parametro props alla mia funzione
-function Menu(props) {
 
-    //aggiorno la mia lista ul utilizzando props e creato l'array di oggetti con chiave "link" e "url" (spostato in App()), vado ad inserire il tag <a> con href su .url dell'elemento in posizione [0 ---> length - 1]
+function MenuHeader({ linkList }) {
     return (<ul>
-        <li><a href={props.linkList[0].url}></a>{props.linkList[0].link}</li>
-        <li><a href={props.linkList[1].url}></a>{props.linkList[1].link}</li>
-        <li><a href={props.linkList[2].url}></a>{props.linkList[2].link}</li>
-        <li><a href={props.linkList[3].url}></a>{props.linkList[3].link}</li>
-        <li><a href={props.linkList[4].url}></a>{props.linkList[4].link}</li>
-        <li><a href={props.linkList[5].url}></a>{props.linkList[5].link}</li>
-        <li><a href={props.linkList[6].url}></a>{props.linkList[6].link}</li>
-        <li><a href={props.linkList[7].url}></a>{props.linkList[7].link}</li>
-        <li><a href={props.linkList[8].url}></a>{props.linkList[8].link}</li>
-        <li><a href={props.linkList[9].url}></a>{props.linkList[9].link}</li>
+        {linkList.map(element => <li key={element.id}><a href={element.url}></a>{element.link}</li>)}
     </ul>
 
     )
 }
-
-export default Menu;
-
-/*<ul>
-        <li>Characters</li>
-        <li>Comics</li>
-        <li>Movies</li>
-        <li>TV</li>
-        <li>Games</li>
-        <li>Collectibles</li>
-        <li>Videos</li>
-        <li>Fans</li>
-        <li>News</li>
-        <li>Shop</li>
-    </ul> */
+export default MenuHeader;
